2076ff8da0412cfeb6c3a2dff340d17aa428c482
Reginald de Wasseige <reginalddewasseige@gmail.com>
2018-02-22 16:45:07 +0100

    added preview on add an article

---
+++ app/assets/javascripts/documentpreview.js
@@ -0,0 +1,5 @@
+function PreviewImage() {
+                pdffile=document.getElementById("uploadPDF").files[0];
+                pdffile_url=URL.createObjectURL(pdffile);
+                $('#viewer').attr('src',pdffile_url);
+            }
