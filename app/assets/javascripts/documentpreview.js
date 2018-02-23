a225a79cc55ffeef210023b080095260a3661c1c
Reginald de Wasseige <reginalddewasseige@gmail.com>
2018-02-23 09:28:29 +0100

    changed slightly the viewer

--- app/assets/javascripts/documentpreview.js
+++ app/assets/javascripts/documentpreview.js
@@ -1,5 +1,5 @@
 function PreviewImage() {
-                pdffile=document.getElementById("uploadPDF").files[0];
+                pdffile=document.getElementById("input").files[0];
                 pdffile_url=URL.createObjectURL(pdffile);
                 $('#viewer').attr('src',pdffile_url);
             }
